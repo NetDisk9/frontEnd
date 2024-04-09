5252e9ea16ba695a3d43036cb70c2824fbeaba34
林铭耀 <linmingyao@linmingyaodeMacBook-Pro.local>
2024-04-09 08:46:01 +0800

    change homeview

--- netdisk/src/views/HomeView.vue
+++ netdisk/src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <template>
   <!-- 个人中心界面 -->
   <div>
-  <div id="top-bar">
-    <div class="logo"></div>
-       </div>
+    <div id="top-bar">
+      <div class="logo"></div>
+    </div>
     <div id="divider"></div>
     <div id="content">
       <div id="area1" class="area">
@@ -19,35 +19,43 @@
       </div>
       <div id="gap"></div>
       <div id="area2" class="area">
-          <div v-if="show===1" style="overflow: auto">
-            <EnterHistory/>
-          </div>
-          <div v-if="show===2" style="overflow: auto">
-            <Devices/>
-          </div>
-          <div v-if="show===3" style="overflow: auto">
-              <changeCode/>
-          </div>
-          <div v-if="show===4" style="overflow: auto">
-              <loginMethods/>
-          </div>
+        <div v-if="show===1" style="overflow: auto">
+          <EnterHistory/>
+        </div>
+        <div v-if="show===2" style="overflow: auto">
+          <Devices/>
+        </div>
+        <div v-if="show===3" style="overflow: auto">
+          <changeCode/>
+        </div>
+        <div v-if="show===4" style="overflow: auto">
+          <loginMethods/>
+        </div>
       </div>
       <div id="gap"></div>
-      <div id="area3" class="area">
-          <div id="avatar-preview" :style="{backgroundImage: 'url(' + avatarPreviewUrl + ')'}">
-          <div class="upload-container">
+      <div id="area3">
+        <div style="display: flex; justify-content: center; align-items: center; margin: 20px 5px 20px 20px">
+          <div style="display: flex; justify-content: center; align-items: center;">
+            <div id="avatar-preview" :style="{backgroundImage: 'url(' + avatarPreviewUrl + ')'}"/>
+          </div>
+          <div class="upload-container" style="display: flex;">
             <label for="avatar-input" id="avatar-upload" @change="handleAvatarChange">修改</label>
             <input type="file" id="avatar-input" @change="handleAvatarChange">
           </div>
         </div>
-        <div id="nickname">昵称</div>
-        <div style="width: 50px" id="nickname-edit" @click="handleNicknameEdit">修改</div>
-        <div id="user-id">user_id</div>
+
+        <div style="display: flex; flex-direction: column; justify-content: center; align-items: center;">
+          <div style="display: flex; justify-content: center; align-items:center; width: 100%" >
+            <div id="nickname" style="display: flex; justify-content: center; margin-left: 20px">昵称</div>
+            <div id="nickname-edit" @click="handleNicknameEdit" style="font-size: 16px;">修改</div>
+          </div>
+          <div id="user-id" style="display: flex">user_id</div>
+        </div>
       </div>
       <input type="file" id="avatar-input" accept="image/*">
     </div>
   </div>
-  </template>
+</template>
 
 <script>
 import Devices from '@/components/devices.vue'
@@ -184,189 +192,188 @@ export default {
 }
 </script>
 
-  <style scoped>
-  body {
-    margin: 0;
-    padding: 0;
-    background-color: white;
-    font-family: Arial, sans-serif;
-  }
-  #top-bar {
-    width: 100%;
-    height: 70px;
-    background-color: white;
-  }
-  #divider {
-    width: 100%;
-    border-bottom: 2px solid black;
-  }
-  .logo {
-    width:170px ;
-    height: 70px;
-    margin-left: 2%;
-    margin-bottom: -2.35%;
-    background-image: url('../assets/logo2.png');
-    background-size:100% 100% ;
-  }
-  #content {
-    width: 100%;
-    display: flex;
-    justify-content: space-between;
-    height: 100vh;
-  }
-  .area {
-    height: 100%;
-    display: flex;
-    justify-content: center;
-    position: relative;
-  }
-  #area1 {
-    width: 14%;
-    background-color: white;
-  }
-  #area2 {
-    width: 64%;
-    background-color: white;
-  }
-  #area3 {
-    width: 18%;
-    background-color: white;
-  }
-  #gap {
-    width: 3%;
-    background-color: #F2F4FA;
-  }
-  nav ul {
-    padding: 0;
-    margin: 0;
-    list-style-type: none;
-  }
-  nav ul li a {
-    display: block;
-    padding: 13px;
-    padding-left: 40px;
-    padding-top: 27px;
-    color: #4E5969;
-    text-decoration: none;
-    font-size: 20px;
-    font-weight: bold;
-    margin-right: 5px;
-    background-size: 25px;
-    background-repeat: no-repeat;
-    background-position: -2px 28px ;
-  }
-  /* Define different icons for each navigation item */
-  nav ul li:nth-child(1) a {
-    background-image: url('@/assets/个人中心.png');
-  }
-  nav ul li:nth-child(2) a {
-    background-image: url('@/assets/更改密码.png');
-  }
-  nav ul li:nth-child(3) a {
-    background-image: url('@/assets/登陆记录.png');
-  }
-  nav ul li:nth-child(4) a {
-    background-image: url('@/assets/登录设备.png');
-  }
-  nav ul li:nth-child(5) a {
-    background-image: url('@/assets/登录方式.png');
-  }
-  #avatar-preview {
-    width: 85%;
-    height: 13%;
-    border-radius: 50%;
-    background-color: #ccc;
-    background-size: cover;
-    background-position: center;
-    position: relative;
-    bottom: -5%;
-    right: -28%;
-  }
-  #avatar-preview .upload-container {
-    position: relative;
-    right: -100%;
-    bottom: -80%;
-    display: flex;
-    align-items: center;
-  }
-  #avatar-upload {
-    color: #2881F6;
-    cursor: pointer;
-    margin-right: 5px;
-  }
-  #nickname {
-    position: relative;
-    width: 38%;
-    bottom: -21%;
-    right: 0%;
-    color: black;
-    font-size: 20px;
-    font-weight: bold;
-  }
-  #nickname-edit {
-    position: relative;
-    width: 29%;
-    bottom: -21.465%;
-    right: -6%;
-    color: #2881F6;
-    cursor: pointer;
-    text-decoration: none;
-  }
-  #user-id {
-    position: relative;
-    width: 58%;
-    bottom: -27%;
-    right: 34%;
-    color: black;
-    font-size: 16px;
-    font-weight: bold;
-  }
-  #avatar-input {
-    display: none; /* Hide the file input */
-  }
-  nav ul li a.selected {
-    color: blue; /* 改变选中链接的文本颜色 */
-  }
-  /* 在选中链接下方添加小箭头 */
-  nav ul li a.selected::after {
-    content: ""; /* 添加伪元素 */
-    display: block; /* 将伪元素设为块级元素 */
-    width: 0;
-    height: 0;
-    border-left: 5px solid transparent;
-    border-right: 5px solid transparent;
-    border-top: 5px solid blue; /* 小箭头颜色与选中链接文本颜色相同 */
-    position: absolute;
-    bottom: -8px; /* 控制小箭头距离链接的距离 */
-    left: 50%; /* 将小箭头水平居中 */
-    transform: translateX(-50%); /* 将小箭头水平居中 */
-    content: '';
-  }
-  .middleBox {
-    margin: 50px auto;
-    width: 60%;
-    height: 600px;
-    /* background-color: #f2f2f2; */
-    /* border: 2px solid red; */
-    border-radius: 10px;
-    padding: 20px;
-    box-sizing: border-box;
-    margin-right: 250px;
-  }
-  /* 内容样式 */
-  .contentmima {
-    padding-left: 5%;
-  }
-  /* 标题样式 */
-  h2 {
-    margin-top: 20px;
-    margin-bottom: 10px;
-    color: #333;
-  }
-  h3 {
-    margin-top: 20px;
-    margin-bottom: 10px;
-    color: #333;
-    font-weight: lighter;
-  }
-  </style>
+<style scoped>
+body {
+  margin: 0;
+  padding: 0;
+  background-color: white;
+  font-family: Arial, sans-serif;
+}
+#top-bar {
+  min-height: 65px;
+  width: 100%;
+  height: 65px;
+  background-color: white;
+}
+#divider {
+  width: 100%;
+  border-bottom: 2px solid black;
+}
+.logo {
+  width:160px ;
+  height: 50px;
+  margin-top: 10px;
+  margin-left: 2%;
+  margin-bottom: -2.35%;
+  background-image: url('../assets/logo2.png');
+  background-size:100% 100% ;
+}
+#content {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  height: 100vh;
+}
+.area {
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  position: relative;
+}
+#area1 {
+  min-width: 180px;
+  width: 180px;
+  background-color: white;
+}
+#area2 {
+  width: 64%;
+  background-color: white;
+}
+#area3 {
+  width: 160px;
+  min-width: 160px;
+  background-color: white;
+}
+#gap {
+  width: 3%;
+  background-color: #F2F4FA;
+}
+nav ul {
+  padding: 0;
+  margin: 0;
+  list-style-type: none;
+}
+nav ul li a {
+  display: block;
+  padding: 13px;
+  padding-left: 40px;
+  padding-top: 27px;
+  color: #4E5969;
+  text-decoration: none;
+  font-size: 20px;
+  font-weight: bold;
+  margin-right: 5px;
+  background-size: 25px;
+  background-repeat: no-repeat;
+  background-position: -2px 28px ;
+}
+/* Define different icons for each navigation item */
+nav ul li:nth-child(1) a {
+  background-image: url('@/assets/个人中心.png');
+}
+nav ul li:nth-child(2) a {
+  background-image: url('@/assets/更改密码.png');
+}
+nav ul li:nth-child(3) a {
+  background-image: url('@/assets/登陆记录.png');
+}
+nav ul li:nth-child(4) a {
+  background-image: url('@/assets/登录设备.png');
+}
+nav ul li:nth-child(5) a {
+  background-image: url('@/assets/登录方式.png');
+}
+#avatar-preview {
+  width: 90px;
+  height: 90px;
+  border-radius: 50%;
+  background-color: #ccc;
+  background-size: cover;
+  background-position: center;
+  position: relative;
+  left: 0;
+  bottom: 0;
+}
+.upload-container {
+  display: flex;
+  margin-left: auto;
+}
+#avatar-upload {
+  color: #2881F6;
+  cursor: pointer;
+  width: 32px;
+}
+#nickname {
+  color: black;
+  font-size: 20px;
+  font-weight: bold;
+  width: 40px;
+}
+
+#nickname-edit {
+  color: #2881F6;
+  cursor: pointer;
+  text-decoration: none;
+}
+
+#user-id {
+  margin: 10px;
+  color: black;
+  font-size: 16px;
+  font-weight: bold;
+  position: relative;
+  left: 0;
+  bottom: 0;
+  top: 0;
+  right: 0;
+  width: auto;
+}
+#avatar-input {
+  display: none; /* Hide the file input */
+}
+nav ul li a.selected {
+  color: blue; /* 改变选中链接的文本颜色 */
+}
+/* 在选中链接下方添加小箭头 */
+nav ul li a.selected::after {
+  content: ""; /* 添加伪元素 */
+  display: block; /* 将伪元素设为块级元素 */
+  width: 0;
+  height: 0;
+  border-left: 5px solid transparent;
+  border-right: 5px solid transparent;
+  border-top: 5px solid blue; /* 小箭头颜色与选中链接文本颜色相同 */
+  position: absolute;
+  bottom: -8px; /* 控制小箭头距离链接的距离 */
+  left: 50%; /* 将小箭头水平居中 */
+  transform: translateX(-50%); /* 将小箭头水平居中 */
+  content: '';
+}
+.middleBox {
+  margin: 50px auto;
+  width: 60%;
+  height: 600px;
+  /* background-color: #f2f2f2; */
+  /* border: 2px solid red; */
+  border-radius: 10px;
+  padding: 20px;
+  box-sizing: border-box;
+  margin-right: 250px;
+}
+/* 内容样式 */
+.contentmima {
+  padding-left: 5%;
+}
+/* 标题样式 */
+h2 {
+  margin-top: 20px;
+  margin-bottom: 10px;
+  color: #333;
+}
+h3 {
+  margin-top: 20px;
+  margin-bottom: 10px;
+  color: #333;
+  font-weight: lighter;
+}
+</style>
